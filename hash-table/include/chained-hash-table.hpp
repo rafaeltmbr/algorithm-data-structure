d2a838f352667fe166cc361fa7031b9c4db90abb
Rafhael Payão <rafaeltmbr@gmail.com>
2019-07-11 23:28:14 -0300

    Hash function constructed

--- hash-table/include/chained-hash-table.hpp
+++ hash-table/include/chained-hash-table.hpp
@@ -9,23 +9,30 @@ typedef int (*hash_t)(const void *key);
 class ChainedHashTable {
 protected:
     int size;
+    int dataSize;
+    int keys;
     match_t match;
     destroy_t destroyData;
     hash_t hash;
 public:
     ChainedHashTable();
-    ChainedHashTable(hash_t, match_t = nullptr, destroy_t = nullptr);
-    ChainedHashTable(ChainedHashTable&);
+    ChainedHashTable(
+        hash_t hash,
+        match_t matchFunc = nullptr,
+        destroy_t destroyFunc = nullptr,
+        int keys = 23,
+        int dataSize = 4 );
+    ChainedHashTable(ChainedHashTable& chtable);
     ~ChainedHashTable();
-    void setDestroy(destroy_t destroyFunc) {destroyData = destroyFunc; };
-    void setMatch(match_t match) { this->match = match; }
-    void setHash(hash_t hash) { this->hash = hash; }
+    void setDestroy(destroy_t destroyFunc) { destroyData = destroyFunc; };
+    void setMatch(match_t matchFunc) { this->match = matchFunc; }
+    void setHash(hash_t hashFunc) { this->hash = hashFunc; }
     void destroy(void);
-    void destroy(destroy_t);
+    void destroy(destroy_t destroyFunc);
     bool insert(void *data);
     void* remove(void *data);
     void* lookup(void *data);
-    int getSize(void);
+    int getSize(void) { return size; };
 };
 
 #endif // CHAINED_HASH_TABLE_HPP
